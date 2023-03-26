1fb9371d2b214ca8c33e886fcdee2b7983c3492f
Anil Madhavapeddy <anil@recoil.org>
2023-03-26 14:53:13 +0000

    Revert "test: gather basic read/write latency info"
    This reverts commit 32216fb3dd9edb9050d7937557699973c428db97.

--- test/copy_lib.ml
+++ test/copy_lib.ml
@@ -5,7 +5,6 @@ type request = {
   buf: Cstruct.buffer;
   off: Optint.Int63.t;
   req: req;
-  ts: int64;
 }
 
 (* The maximum number of threads that the operating system can allow to concurrently process I/O completion packets for the I/O completion port.
@@ -13,7 +12,7 @@ type request = {
 *)
 let num_threads = 0
 
-let copy_file ?read_latency ?write_latency block_size queue_depth infile outfile =
+let copy_file block_size queue_depth infile outfile =
   let iocp = Iocp.Safest.create num_threads in
   let fd = Iocp.Safest.openfile iocp 0 infile [ O_RDONLY ] 0 in
   let out =
@@ -27,14 +26,12 @@ let copy_file ?read_latency ?write_latency block_size queue_depth infile outfile
     if Optint.Int63.compare next_read_off st_size < 0 && num_in_flight < queue_depth then begin
       let buf = Cstruct.(to_bigarray @@ create block_size) in
       (* Printf.fprintf stderr "initial submit off=%d in_flight_requests = %d\n" (Optint.Int63.to_int next_read_off) num_in_flight; *)
-      let request = {
+      let id = Iocp.Safest.read iocp fd buf ~pos:0 ~off:next_read_off ~len:block_size in
+      Iocp.Safest.H.replace in_progress_requests id {
         buf = buf;
         off = next_read_off;
         req = `R;
-        ts = Mtime_clock.now_ns ();
-      } in
-      let id = Iocp.Safest.read iocp fd buf ~pos:0 ~off:next_read_off ~len:block_size in
-      Iocp.Safest.H.replace in_progress_requests id request;
+      };
       submit_initial_requests Optint.Int63.(add next_read_off (of_int block_size)) (num_in_flight + 1)
     end else next_read_off, num_in_flight in
   let next_read_off, num_in_flight = submit_initial_requests Optint.Int63.zero 0 in
@@ -50,30 +47,19 @@ let copy_file ?read_latency ?write_latency block_size queue_depth infile outfile
         begin match request.req with
         | `R ->
           (* Printf.fprintf stderr "read completed at %d\n" (Optint.Int63.to_int request.off); *)
-          begin match read_latency with
-          | None -> ()
-          | Some l -> Latency.add l (Int64.sub (Mtime_clock.now_ns ()) request.ts)
-          end;
-          let request = { request with req = `W; ts = Mtime_clock.now_ns () } in
           let id = Iocp.Safest.write iocp out request.buf ~pos:0 ~off:request.off ~len:t.bytes_transferred in
-          Iocp.Safest.H.replace in_progress_requests id request;
+          Iocp.Safest.H.replace in_progress_requests id { request with req = `W };
           handle_completion next_read_off num_in_flight
         | `W ->
           (* Printf.fprintf stderr "write completed at %d\n" (Optint.Int63.to_int request.off); *)
-          begin match write_latency with
-          | None -> ()
-          | Some l -> Latency.add l (Int64.sub (Mtime_clock.now_ns ()) request.ts)
-          end;
           if Optint.Int63.compare next_read_off st_size < 0 then begin
             let buf = Cstruct.(to_bigarray @@ create block_size) in
-            let request = {
+            let id = Iocp.Safest.read iocp fd buf ~pos:0 ~off:next_read_off ~len:block_size in
+            Iocp.Safest.H.replace in_progress_requests id {
               buf = buf;
               off = next_read_off;
               req = `R;
-              ts = Mtime_clock.now_ns ();
-            } in
-            let id = Iocp.Safest.read iocp fd buf ~pos:0 ~off:next_read_off ~len:block_size in
-            Iocp.Safest.H.replace in_progress_requests id request;
+            };
             handle_completion Optint.Int63.(add next_read_off (of_int block_size)) num_in_flight
           end else handle_completion next_read_off (num_in_flight - 1)
         end
@@ -84,5 +70,5 @@ let copy_file ?read_latency ?write_latency block_size queue_depth infile outfile
   Unix.close @@ Iocp.Handle.fd fd;
   Unix.close @@ Iocp.Handle.fd out
 
-let run_cp ?read_latency ?write_latency block_size queue_depth infile outfile () =
-  copy_file ?read_latency ?write_latency block_size queue_depth infile outfile
+let run_cp block_size queue_depth infile outfile () =
+  copy_file block_size queue_depth infile outfile
